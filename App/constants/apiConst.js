d58608e4e846faa4afb0a19edc7e5f0dbd2bfcc0
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-03-14 21:50:42 +0100

    cambio en constantes

--- App/constants/apiConst.js
+++ App/constants/apiConst.js
@@ -1,5 +1,8 @@
+const ip = "80.39.50.206";
+const uri = `http://${ip}:8082/api/`;
+
 export default {
-  login: "http://83.40.114.7:8082/api/login",
-  register: "http://83.40.114.7:8082/api/register",
-  all: "http://83.40.114.7:8082/api/all",
+  login: uri + login,
+  register: uri + "register",
+  all: uri + "all",
 };
