6e61c3f89c98718ae3284be90402e0819c593562
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-03-14 21:57:24 +0100

    correccion en apiConst

--- App/constants/apiConst.js
+++ App/constants/apiConst.js
@@ -2,7 +2,7 @@ const ip = "80.39.50.206";
 const uri = `http://${ip}:8082/api/`;
 
 export default {
-  login: uri + login,
+  login: uri + "login",
   register: uri + "register",
   all: uri + "all",
 };
