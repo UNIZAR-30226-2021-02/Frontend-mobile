41adb5be51621f2224790654bf347b819857aed5
dolansete <dolancc00@gmail.com>
2021-03-23 11:07:54 +0100

    Añadidas apiuris de amigos

--- App/constants/apiUris.js
+++ App/constants/apiUris.js
@@ -4,5 +4,11 @@ const uri = `http://${ip}:8080/api/`;
 export default {
   login: uri + "login",
   register: uri + "register",
+  acceptRequest: uri + "acceptRequest",
+  sendRequest: uri + "sendRequest",
+  denyRequest: uri + "denyRequest",
+  listRequest: uri + "listRequest",
+  listFriends: uri + "listFriends",
+  deleteFriend: uri + "deleteFriend",
   all: uri + "all",
 };
--- App/screens/FriendsScreen.js
+++ App/screens/FriendsScreen.js
@@ -20,6 +20,7 @@ const styles = StyleSheet.create({
 
 const initialState = {
   searchUser: "",
+  myName: "",
   errors: {},
 };
 
