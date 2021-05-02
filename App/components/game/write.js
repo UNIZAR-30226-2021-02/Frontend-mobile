927bc30de9e1b1d565f378d20c218d7c0a4749da
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-05-03 00:25:17 +0200

    no probado

--- App/components/game/write.js
+++ App/components/game/write.js
@@ -67,7 +67,7 @@ class Write extends Component {
     // Show spinner when call is made
     this.setState({ isLoading: true });
 
-    APIKit.post(URI.addRespuesta, payload).then(onSuccess).catch(onFailure);
+    APIKit.post(URI.addText, payload).then(onSuccess).catch(onFailure);
   }
 
   render() {
