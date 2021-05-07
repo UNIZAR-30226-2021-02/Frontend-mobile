24cac5036bf4b56aed94d56369dd7382551f0d74
Íñigo Aréjula Aísa <arejula27@gmail.com>
2021-05-07 23:15:11 +0200

    preparado votar

--- App/components/votaciones/votacion.js
+++ App/components/votaciones/votacion.js
@@ -4,30 +4,121 @@ import { RowSeparator } from "../RowItem";
 import globalStyles from "../../constants/styles";
 import Grid from 'react-native-grid-component';
 import URI from "../../constants/apiUris"
+import APIKit from "../../util/APIKit"
 
 
 class Votacion extends Component {
 
     constructor(){
+        
         super();
+        this.state={mode:0,players:[]}
+        this.mode=0;
+     
+        
     }
 
-    _renderItem = (data, i) => (
+    componentDidMount(){
+      this.loadData()
+    }
+
+    loadData(){
+
+      const onSuccess = ({ data }) => {
+        //console.log(data)
+     
+        this.setState({players: data});
+        
+   
+      }
+  
+      const onFailure = (error) => {
+        console.log("Ha falladooooooooo")
+        
+       
+        console.log(error);
+      };
+  
+   
+  
+      APIKit.get(URI.listPlayers).then(onSuccess).catch(onFailure);
+    }
+
+    mandarVoto(usr,kind){
+
+      const payload = JSON.stringify({ usr:usr});
+
+      const onSuccess = ({ data }) => {
+
+
+      
+     
+        this.setState({players: data});
+        
+   
+      }
+  
+      const onFailure = (error) => {
+        console.log("Ha falladooooooooo")
+        
+       
+        console.log(error);
+      };
+  
+   
+  
+     
+      //APIKit.post(kind, payload).then(onSuccess).catch(onFailure);
+
+
+
+    }
+
+    votar(usr){
+
+      switch (this.mode) {
+        case 0: //mejor dibujo
+        console.log("dibujante "+usr)
+        this.mandarVoto(usr,URI.voteDibujante)
+        break;
+
+        case 1://más inteligente
+        console.log("clever "+usr)
+        this.mandarVoto(usr,URI.voteListo)
+        break;
+
+        case 2://más divertido
+        console.log("diver "+usr)
+        this.mandarVoto(usr,URI.voteGracioso)
+        //ir a resultados
+        break;
+
+        default:
+          console.log("no debo estar aki")
+
+        break;
+      }
+      this.mode++;
+      
+    }
+
+    _renderItem = (item, i) => (
         <View style={[ styles.item]} key={i} >
-          <TouchableOpacity>
+          <TouchableOpacity onPress={()=>this.votar(item.mail)}>
         <Image
             source={{
-              uri:  URI.img+"foto0.png",
+              uri:  URI.img+item.fotPerf,
             }}
             style={styles.picture}
           />
-          <Text>María</Text>
+          <Text>{item.nombre}</Text>
           </TouchableOpacity>
           </View>
       );
 
       renderPlaceholder = i => <View style={{ backgroundColor: "red" },styles.item} key={i}><Text>María</Text> </View> ;
     render(){
+      
         return (
           <View style={styles.container}>
           <Text>Mejor sapo</Text>
@@ -36,7 +127,7 @@ class Votacion extends Component {
               keyExtractor={(item) => String(item)} //TODO
               renderItem={this._renderItem}
               renderPlaceholder={this._renderPlaceholder}
-              data={['black', 'white', 'red', 'pink', 'blue','blue', ]}
+              data={this.state.players}
               numColumns={5}
             />
             </View>
